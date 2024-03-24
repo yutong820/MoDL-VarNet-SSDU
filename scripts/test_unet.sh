b75df8f91c7735c96715cca42197e3c531843f3c
Yutong Luo <luoyutong1230@gmail.com>
2024-03-24 09:43:47 +0100

    delete unet

--- scripts/test_unet.sh
+++
@@ -1,9 +0,0 @@
-GPU_NUM=0
-TEST_CONFIG_YAML="configs/base_unet,k=1.yaml"
-TENSORBOARD_DIR="./results/unet/folder_2"
-
-CUDA_VISIBLE_DEVICES=$GPU_NUM python test.py \
-    --config=$TEST_CONFIG_YAML \
-    --batch_size=32 \
-    --write_image=1 \
-    --tensorboard_dir=$TENSORBOARD_DIR
