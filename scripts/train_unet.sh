f3ef6d2b07b5fa7c88107dae5f6b74aa2e772019
Yutong Luo <luoyutong1230@gmail.com>
2024-03-24 09:44:23 +0100

    delete unet

--- scripts/train_unet.sh
+++
@@ -1,8 +0,0 @@
-GPU_NUM=0
-TRAIN_CONFIG_YAML="configs/base_unet,k=1.yaml"
-TENSORBOARD_DIR="./results/unet/folder_2"
-
-CUDA_VISIBLE_DEVICES=$GPU_NUM python train.py \
-    --config=$TRAIN_CONFIG_YAML \
-    --write_image=10 \
-    --tensorboard_dir=$TENSORBOARD_DIR
